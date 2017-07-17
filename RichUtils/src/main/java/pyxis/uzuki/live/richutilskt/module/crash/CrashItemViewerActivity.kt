984023d8b33a4e25e4bff821696a25e48884ede7
Pyxis <pyxis@uzuki.live>
2017-07-17 17:33:01 +0900

    야호 퇴근이다

--- RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/module/crash/CrashItemViewerActivity.kt
+++ RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/module/crash/CrashItemViewerActivity.kt
@@ -2,6 +2,9 @@ package pyxis.uzuki.live.richutilskt.module.crash
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import pyxis.uzuki.live.richutilskt.R
+import pyxis.uzuki.live.richutilskt.utils.RPreference
 
 /**
  * RichUtilsKt
@@ -9,10 +12,18 @@ import android.support.v7.app.AppCompatActivity
  * Created by winds on 2017-07-17.
  */
 
-class CrashItemViewerActivity : AppCompatActivity() {
+abstract class CrashItemViewerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crashitem_viewer)
+
+        val logLocation = RPreference.getInstance(this).getString("logLocation")
+        if (TextUtils.isEmpty(logLocation))
+           displayEnterLocationDialog()
+    }
+
+    private fun displayEnterLocationDialog() {
 
     }
 }
